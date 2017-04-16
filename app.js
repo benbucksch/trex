5063acd40254894818f231809c43aac8020b0d06
Ben Bucksch <ben.bucksch@beonex.com>
2017-04-16 15:31:13 +0200

    Sample app with debug output

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
+/**
+ * Sample app, demo purposes only.
+ */
+
 function onLoad() {
- var gStrings = new StringBundle("sample");
- translateElements(document, gStrings, {"brand": "TAppKit" });
+  try {
+    var gStrings = new StringBundle("sample");
+    translateElements(document, gStrings, {"brand": "TAppKit" });
+
+    var box = document.getElementById("intro-box");
+    box.trex.hidden = true;
+  } catch (e) { errorCritical(e); }
 }
 window.addEventListener("load", onLoad, false);
+
+
+function errorCritical(msg) {
+  console.error(msg);
+  alert(msg);
+}
+
+function errorNonCritical(msg) {
+  console.error(msg);
+}
